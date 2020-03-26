004cbe8b0e55d3daf86a9f7d4d992f16590df3ab
Ivan Yelizariev <yelizariev@itpp.dev>
2020-03-26 16:44:52 +0500

    :arrow_up::one::three: pos_debranding

--- pos_debranding/__manifest__.py
+++ pos_debranding/__manifest__.py
@@ -1,15 +1,17 @@
+# Copyright 2015-2018,2020 Ivan Yelizariev
+# License OPL-1 (https://www.odoo.com/documentation/user/13.0/legal/licenses/licenses.html#odoo-apps) for derivative work.
 {
     "name": "POS debranding",
     "version": "13.0.1.0.0",
     "author": "IT-Projects LLC, Ivan Yelizariev",
-    "license": "Other OSI approved licence",  # MIT
+    "license": "OPL-1",
     "category": "Debranding",
     "support": "pos@it-projects.info",
     "website": "https://www.odoo.com/apps/modules/13.0/pos_debranding/",
     "depends": ["point_of_sale"],
     # 'price': 30.00,
     # 'currency': 'EUR',
-    "data": ["views.xml", "template.xml"],
+    "data": ["template.xml"],
     "qweb": ["static/src/xml/pos_debranding.xml"],
-    "installable": False,
+    "installable": True,
 }
